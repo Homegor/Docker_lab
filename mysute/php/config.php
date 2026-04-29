3ff2cd2bb5061aa8309e8ae0bbe63df8000de5b2
Khomyakov Egor <homegor14@gmail.com>
2026-04-29 20:38:47 +0300

    rename site build docker site problem utf-8

---
+++ mysute/php/config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php - Конфигурация подключения к базе данных
+define('DB_HOST', 'db');
+define('DB_USER', 'root');        // ваш пользователь MySQL
+define('DB_PASS', 'root');            // ваш пароль MySQL
+define('DB_NAME', 'test_db');     // имя вашей базы данных
+?>
